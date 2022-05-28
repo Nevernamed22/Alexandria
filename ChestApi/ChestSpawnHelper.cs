1e9dc3c2b67888ff0a72a32bf607b2fa369690b7
notabot <12131850+nota8ot@users.noreply.github.com>
2022-05-29 05:40:47 +1000

    fucking just do what im telling you too you piece of shit

---
+++ ChestApi/ChestSpawnHelper.cs
@@ -0,0 +1,68 @@
+﻿using Dungeonator;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ChestApi
+{
+    public static class ChestSpawnHelper
+    {
+        public static void Init()
+        {
+            var ConfigureOnPlacementHook = new Hook(
+                    typeof(FloorChestPlacer).GetMethod("ConfigureOnPlacement", BindingFlags.Instance | BindingFlags.Public),
+                    typeof(ChestSpawnHelper).GetMethod("ConfigureOnPlacementHook", BindingFlags.Static | BindingFlags.Public));
+		}
+
+		public static void RegisterCustomChest(Chest chest, float weight = 1)
+        {
+
+		}
+
+		//d 0.25
+		//c 0.32
+		//b 0.2
+		//a 0.09
+		//s 0.04
+
+		public static void ConfigureOnPlacementHook(Action<FloorChestPlacer, RoomHandler> orig, FloorChestPlacer self, RoomHandler room)
+		{
+
+			
+			var chest = GetTargetCustomChest(UnityEngine.Random.value);
+			if (chest)
+			{
+				self.OverrideChestPrefab = chest;
+				self.UseOverrideChest = true;
+				DungeonPrerequisite dungeonPrerequisite = new DungeonPrerequisite();
+				dungeonPrerequisite.requireTileset = true;
+				dungeonPrerequisite.requiredTileset = GlobalDungeonData.ValidTilesets.CASTLEGEON;
+				dungeonPrerequisite.prerequisiteType = DungeonPrerequisite.PrerequisiteType.TILESET;
+			}
+			orig(self, room);
+		}
+
+		public static Chest GetTargetCustomChest(float fran)
+		{
+			//float currentMagnificence = GameManager.Instance.RewardManager.CurrentRewardData.DetermineCurrentMagnificence(isGenerationForMagnificence);
+
+			float dumbFuckingChestNumber = 0;
+			foreach(var chest in customChests)
+            {
+				dumbFuckingChestNumber += chest.Value.Second;
+				if (fran < dumbFuckingChestNumber)
+				{
+					return chest.Value.First;
+				}
+			}
+			return null;
+		}
+
+
+		public static Dictionary<string, Tuple<Chest, float>> customChests = new Dictionary<string, Tuple<Chest, float>>();
+
+	}
+}
---
+++ Miscellaneous Helpers/MiscHooks.cs
@@ -0,0 +1,36 @@
+﻿using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Alexandria
+{
+    public static class MiscHooks
+    {
+        public static void Init()
+        {
+            var ChangeToRandomGunHook = new Hook(
+                    typeof(PlayerController).GetMethod("ChangeToRandomGun", BindingFlags.Instance | BindingFlags.Public),
+                    typeof(MiscHooks).GetMethod("ChangeToRandomGunHook", BindingFlags.Static | BindingFlags.Public));
+
+        }
+
+        public static void ChangeToRandomGunHook(Action<PlayerController> orig, PlayerController self)
+		{
+            orig(self);
+            if (GameManager.Instance.CurrentLevelOverrideState == GameManager.LevelOverrideState.END_TIMES || GameManager.Instance.CurrentLevelOverrideState == GameManager.LevelOverrideState.CHARACTER_PAST || GameManager.Instance.CurrentLevelOverrideState == GameManager.LevelOverrideState.TUTORIAL)
+            {
+                return;
+            }
+
+            var currentGun = self.CurrentGun;
+
+            if (currentGun && currentGun.HasTag("exclude_blessed"))
+            {               
+                self.ChangeToRandomGun();
+            }
+        }
+	}
+}

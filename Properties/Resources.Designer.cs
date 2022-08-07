336a04dcd25c310fd68e568d6efd1287bcc92168
An3s079 <An3sferreira@gmail.com>
2022-08-06 16:21:21 -1000

    add NuGet package support

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EtG.Alexandria.Properties {
+namespace Alexandria.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace EtG.Alexandria.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EtG.Alexandria.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Alexandria.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

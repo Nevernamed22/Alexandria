118f08166cf2130554f12d98c1b2e8fb7cce03f3
Some-Bunny <70037543+Some-Bunny@users.noreply.github.com>
2024-07-07 00:31:41 +0100

    Update Module.cs

--- Module.cs
+++ Module.cs
@@ -30,7 +30,7 @@ namespace Alexandria
         public const string GUID = "alexandria.etgmod.alexandria";
         public const string NAME = "Alexandria";
 
-        public const string VERSION = "0.4.6";
+        public const string VERSION = "0.4.5";
 
 
 

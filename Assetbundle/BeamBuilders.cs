fe3e41edfdd675855f03655af419259cc7c37621
Patrick Crain <patrickacrain@gmail.com>
2024-08-12 19:13:39 -0400

    Add GenerateAnchoredBeamPrefabBundle() for setting up beams for sprites that already have anchors set up

--- Assetbundle/BeamBuilders.cs
+++ Assetbundle/BeamBuilders.cs
@@ -10,130 +10,85 @@ namespace Alexandria.Assetbundle
 {
     public static class BeamBuilders
     {
-        public static BasicBeamController GenerateBeamPrefabBundle(this Projectile projectile, string defaultSpriteName, tk2dSpriteCollectionData data, tk2dSpriteAnimation animation, string IdleAnimationName, Vector2 colliderDimensions, Vector2 colliderOffsets, string impactVFXAnimationName = null, Vector2? impactVFXColliderDimensions = null, Vector2? impactVFXColliderOffsets = null, string endAnimation = null, Vector2? endColliderDimensions = null, Vector2? endColliderOffsets = null, string muzzleAnimationName = null, Vector2? muzzleColliderDimensions = null, Vector2? muzzleColliderOffsets = null, bool glows = false,
-            bool canTelegraph = false, string beamTelegraphIdleAnimationName = null, string beamStartTelegraphAnimationName = null, string beamEndTelegraphAnimationName = null, float telegraphTime = 1,
-            bool canDissipate = false, string beamDissipateAnimationName = null, string beamStartDissipateAnimationName = null, string beamEndDissipateAnimationName = null, float dissipateTime = 1)
+        public static BasicBeamController GenerateBeamPrefabBundle(this Projectile projectile, string defaultSpriteName, tk2dSpriteCollectionData data,
+            tk2dSpriteAnimation animation, string IdleAnimationName, Vector2 colliderDimensions, Vector2 colliderOffsets, string impactVFXAnimationName = null,
+            Vector2? impactVFXColliderDimensions = null, Vector2? impactVFXColliderOffsets = null, string endAnimation = null, Vector2? endColliderDimensions = null,
+            Vector2? endColliderOffsets = null, string muzzleAnimationName = null, Vector2? muzzleColliderDimensions = null, Vector2? muzzleColliderOffsets = null,
+            bool glows = false, bool canTelegraph = false, string beamTelegraphIdleAnimationName = null, string beamStartTelegraphAnimationName = null,
+            string beamEndTelegraphAnimationName = null, float telegraphTime = 1, bool canDissipate = false, string beamDissipateAnimationName = null,
+            string beamStartDissipateAnimationName = null, string beamEndDissipateAnimationName = null, float dissipateTime = 1)
         {
-            try
-            {
-                if (projectile.specRigidbody)
-                    projectile.specRigidbody.CollideWithOthers = false;
-
-                tk2dTiledSprite tiledSprite = projectile.gameObject.GetOrAddComponent<tk2dTiledSprite>();
-
-                tiledSprite.Collection = data;
-                tiledSprite.SetSprite(data, data.GetSpriteIdByName(defaultSpriteName));
-                tk2dSpriteDefinition def = tiledSprite.GetCurrentSpriteDef();
-                def.colliderVertices = new Vector3[]{ 0.0625f * colliderOffsets, 0.0625f * colliderDimensions };
-
-                def.ConstructOffsetsFromAnchor(tk2dBaseSprite.Anchor.MiddleLeft); //NOTE: this seems right, but double check later
-
-                tk2dSpriteAnimator animator = projectile.gameObject.GetOrAddComponent<tk2dSpriteAnimator>();
-                animator._startingSpriteCollection = data;
-                animator.Library = animation;
-                animator.playAutomatically = true;
-                animator.defaultClipId = animation.GetClipIdByName(IdleAnimationName);
-
-                UnityEngine.Object.Destroy(projectile.GetComponentInChildren<tk2dSprite>());
-                projectile.sprite = tiledSprite;
-                projectile.sprite.Collection = data;
-
-                BasicBeamController beamController = projectile.gameObject.GetOrAddComponent<BasicBeamController>();
-                beamController.sprite = tiledSprite;
-                beamController.spriteAnimator = animator;
-                beamController.m_beamSprite = tiledSprite;
-
-                //---------------- Sets up the animation for the main part of the beam
-                beamController.beamAnimation = IdleAnimationName;
-
-                //------------- Sets up the animation for the part of the beam that touches the wall
-
-                if (endAnimation != null && endColliderDimensions != null && endColliderOffsets != null)
-                {
-                    SetupBeamPart(animation, data, endAnimation, (Vector2)endColliderDimensions, (Vector2)endColliderOffsets);
-                    beamController.beamEndAnimation = endAnimation;
-                }
-                else
-                {
-                    SetupBeamPart(animation, data, IdleAnimationName, null, null, def.colliderVertices);
-                    beamController.beamEndAnimation = IdleAnimationName;
-                }
-
-                //---------------Sets up the animaton for the VFX that plays over top of the end of the beam where it hits stuff
-                if (impactVFXAnimationName != null && impactVFXColliderDimensions != null && impactVFXColliderOffsets != null)
-                {
-                    SetupBeamPart(animation, data, impactVFXAnimationName, (Vector2)impactVFXColliderDimensions, (Vector2)impactVFXColliderOffsets);
-                    beamController.impactAnimation = impactVFXAnimationName;
-                }
-
-                //--------------Sets up the animation for the very start of the beam
-                if (muzzleAnimationName != null && muzzleColliderDimensions != null && muzzleColliderOffsets != null)
-                {
-                    SetupBeamPart(animation, data, muzzleAnimationName, (Vector2)muzzleColliderDimensions, (Vector2)muzzleColliderOffsets);
-                    beamController.beamStartAnimation = muzzleAnimationName;
-                }
-                else
-                {
-                    SetupBeamPart(animation, data, IdleAnimationName, null, null, def.colliderVertices);
-                    beamController.beamStartAnimation = IdleAnimationName;
-                }
-
-                if (canTelegraph)
-                {
-                    beamController.usesTelegraph = true;
-                    beamController.telegraphAnimations = new BasicBeamController.TelegraphAnims();
-                    if (beamStartTelegraphAnimationName != null)
-                    {
-                        SetupBeamPart(animation, data, beamStartTelegraphAnimationName, Vector2.zero, Vector2.zero);
-                        beamController.telegraphAnimations.beamStartAnimation = beamStartTelegraphAnimationName;
-                    }
-                    if (beamTelegraphIdleAnimationName != null)
-                    {
-                        SetupBeamPart(animation, data, beamTelegraphIdleAnimationName, Vector2.zero, Vector2.zero);
-                        beamController.telegraphAnimations.beamAnimation = beamTelegraphIdleAnimationName;
-                    }
-                    if (beamEndTelegraphAnimationName != null)
-                    {
-                        SetupBeamPart(animation, data, beamEndTelegraphAnimationName, Vector2.zero, Vector2.zero);
-                        beamController.telegraphAnimations.beamEndAnimation = beamEndTelegraphAnimationName;
-                    }
-                    beamController.telegraphTime = telegraphTime;
-                }
-
-                if (canDissipate)
-                {
-                    beamController.endType = BasicBeamController.BeamEndType.Dissipate;
-                    beamController.dissipateAnimations = new BasicBeamController.TelegraphAnims();
-                    if (beamStartDissipateAnimationName != null)
-                    {
-                        SetupBeamPart(animation, data, beamStartDissipateAnimationName, Vector2.zero, Vector2.zero);
-                        beamController.dissipateAnimations.beamStartAnimation = beamStartDissipateAnimationName;
-                    }
-                    if (beamDissipateAnimationName != null)
-                    {
-                        SetupBeamPart(animation, data, beamDissipateAnimationName, Vector2.zero, Vector2.zero);
-                        beamController.dissipateAnimations.beamAnimation = beamDissipateAnimationName;
-                    }
-                    if (beamEndDissipateAnimationName != null)
-                    {
-                        SetupBeamPart(animation, data, beamEndDissipateAnimationName, Vector2.zero, Vector2.zero);
-                        beamController.dissipateAnimations.beamEndAnimation = beamEndDissipateAnimationName;
-                    }
-                    beamController.dissipateTime = dissipateTime;
-                }
-
-                if (glows)
-                    projectile.gameObject.GetOrAddComponent<EmmisiveBeams>();
-                return beamController;
-            }
-            catch (Exception e)
-            {
-                ETGModConsole.Log(e.ToString());
-                return null;
-            }
+            return projectile.GenerateBeamPrefabBundleInternal(
+                defaultSpriteName: defaultSpriteName,
+                data: data,
+                animation: animation,
+                IdleAnimationName: IdleAnimationName,
+                colliderDimensions: colliderDimensions,
+                colliderOffsets: colliderOffsets,
+                impactVFXAnimationName: impactVFXAnimationName,
+                impactVFXColliderDimensions: impactVFXColliderDimensions,
+                impactVFXColliderOffsets: impactVFXColliderOffsets,
+                endAnimation: endAnimation,
+                endColliderDimensions: endColliderDimensions,
+                endColliderOffsets: endColliderOffsets,
+                muzzleAnimationName: muzzleAnimationName,
+                muzzleColliderDimensions: muzzleColliderDimensions,
+                muzzleColliderOffsets: muzzleColliderOffsets,
+                glows: glows,
+                canTelegraph: canTelegraph,
+                beamTelegraphIdleAnimationName: beamTelegraphIdleAnimationName,
+                beamStartTelegraphAnimationName: beamStartTelegraphAnimationName,
+                beamEndTelegraphAnimationName: beamEndTelegraphAnimationName,
+                telegraphTime: telegraphTime,
+                canDissipate: canDissipate,
+                beamDissipateAnimationName: beamDissipateAnimationName,
+                beamStartDissipateAnimationName: beamStartDissipateAnimationName,
+                beamEndDissipateAnimationName: beamEndDissipateAnimationName,
+                dissipateTime: dissipateTime,
+                constructOffsets: true);
         }
 
-        public static void ConstructOffsetsFromAnchor(this tk2dSpriteDefinition def, tk2dBaseSprite.Anchor anchor, Vector2? scale = null, bool fixesScale = false, bool changesCollider = true)
+        /// <summary>Version of GenerateBeamPrefabBundle to use if anchors have already been set up in the asset bundle</summary>
+        public static BasicBeamController GenerateAnchoredBeamPrefabBundle(this Projectile projectile, string defaultSpriteName, tk2dSpriteCollectionData data,
+            tk2dSpriteAnimation animation, string IdleAnimationName, Vector2 colliderDimensions, Vector2 colliderOffsets, string impactVFXAnimationName = null,
+            Vector2? impactVFXColliderDimensions = null, Vector2? impactVFXColliderOffsets = null, string endAnimation = null, Vector2? endColliderDimensions = null,
+            Vector2? endColliderOffsets = null, string muzzleAnimationName = null, Vector2? muzzleColliderDimensions = null, Vector2? muzzleColliderOffsets = null,
+            bool glows = false, bool canTelegraph = false, string beamTelegraphIdleAnimationName = null, string beamStartTelegraphAnimationName = null,
+            string beamEndTelegraphAnimationName = null, float telegraphTime = 1, bool canDissipate = false, string beamDissipateAnimationName = null,
+            string beamStartDissipateAnimationName = null, string beamEndDissipateAnimationName = null, float dissipateTime = 1)
+        {
+            return projectile.GenerateBeamPrefabBundleInternal(
+                defaultSpriteName: defaultSpriteName,
+                data: data,
+                animation: animation,
+                IdleAnimationName: IdleAnimationName,
+                colliderDimensions: colliderDimensions,
+                colliderOffsets: colliderOffsets,
+                impactVFXAnimationName: impactVFXAnimationName,
+                impactVFXColliderDimensions: impactVFXColliderDimensions,
+                impactVFXColliderOffsets: impactVFXColliderOffsets,
+                endAnimation: endAnimation,
+                endColliderDimensions: endColliderDimensions,
+                endColliderOffsets: endColliderOffsets,
+                muzzleAnimationName: muzzleAnimationName,
+                muzzleColliderDimensions: muzzleColliderDimensions,
+                muzzleColliderOffsets: muzzleColliderOffsets,
+                glows: glows,
+                canTelegraph: canTelegraph,
+                beamTelegraphIdleAnimationName: beamTelegraphIdleAnimationName,
+                beamStartTelegraphAnimationName: beamStartTelegraphAnimationName,
+                beamEndTelegraphAnimationName: beamEndTelegraphAnimationName,
+                telegraphTime: telegraphTime,
+                canDissipate: canDissipate,
+                beamDissipateAnimationName: beamDissipateAnimationName,
+                beamStartDissipateAnimationName: beamStartDissipateAnimationName,
+                beamEndDissipateAnimationName: beamEndDissipateAnimationName,
+                dissipateTime: dissipateTime,
+                constructOffsets: false);
+        }
+
+        public static void ConstructOffsetsFromAnchor(this tk2dSpriteDefinition def, tk2dBaseSprite.Anchor anchor, Vector2? scale = null, bool fixesScale = false,
+            bool changesCollider = true)
         {
             Shared.ConstructOffsetsFromAnchor(def, anchor, scale, fixesScale, changesCollider);
         }
@@ -141,11 +96,6 @@ namespace Alexandria.Assetbundle
         public static void MakeOffset(this tk2dSpriteDefinition def, Vector2 offset, bool changesCollider = false)
         {
             Shared.MakeOffset(def, offset, changesCollider);
-        }
-
-        private static void SetupBeamPart(tk2dSpriteAnimation beamAnimation, tk2dSpriteCollectionData data, string animationName, Vector2? colliderDimensions = null, Vector2? colliderOffsets = null, Vector3[] overrideVertices = null, tk2dSpriteAnimationClip.WrapMode wrapMode = tk2dSpriteAnimationClip.WrapMode.Once)
-        {
-            Shared.SetupBeamPart(beamAnimation, data, animationName, colliderDimensions, colliderOffsets, overrideVertices, wrapMode, anchor: tk2dBaseSprite.Anchor.MiddleLeft);
         }
     }
 }
--- Misc/SharedExtensions.cs
+++ Misc/SharedExtensions.cs
@@ -22,8 +22,13 @@ namespace Alexandria.Misc
                 def.colliderVertices[0] += offset;
         }
 
+        private static readonly HashSet<tk2dSpriteDefinition> adjustedDefs = new();
         internal static void ConstructOffsetsFromAnchor(this tk2dSpriteDefinition def, tk2dBaseSprite.Anchor anchor, Vector2? scale = null, bool fixesScale = false, bool changesCollider = true)
         {
+            if (adjustedDefs.Contains(def))
+                return; // don't set up offsets for definitions multiple times
+            adjustedDefs.Add(def);
+
             if (!scale.HasValue)
             {
                 scale = new Vector2?(def.position3);
@@ -198,12 +203,13 @@ namespace Alexandria.Misc
 
         internal static void SetupBeamPart(tk2dSpriteAnimation beamAnimation, tk2dSpriteCollectionData data, string animationName, Vector2? colliderDimensions = null,
             Vector2? colliderOffsets = null, Vector3[] overrideVertices = null, tk2dSpriteAnimationClip.WrapMode wrapMode = tk2dSpriteAnimationClip.WrapMode.Once,
-            tk2dBaseSprite.Anchor anchor = tk2dBaseSprite.Anchor.MiddleLeft)
+            tk2dBaseSprite.Anchor anchor = tk2dBaseSprite.Anchor.MiddleLeft, bool constructOffsets = true)
         {
             foreach (var frame in beamAnimation.GetClipByName(animationName).frames)
             {
                 tk2dSpriteDefinition frameDef = data.spriteDefinitions[frame.spriteId];
-                frameDef.ConstructOffsetsFromAnchor(anchor);
+                if (constructOffsets)
+                    frameDef.ConstructOffsetsFromAnchor(anchor);
                 if (overrideVertices != null)
                     frameDef.colliderVertices = overrideVertices;
                 else if (colliderDimensions != null && colliderOffsets != null)
@@ -231,6 +237,133 @@ namespace Alexandria.Misc
             beamAnimation.clips = beamAnimation.clips.Concat(new tk2dSpriteAnimationClip[] { clip }).ToArray();
             SetupBeamPart(beamAnimation, collection, animationName, colliderDimensions, colliderOffsets, overrideVertices,
                 wrapMode: tk2dSpriteAnimationClip.WrapMode.Once, anchor: anchor);
+        }
+
+        internal static BasicBeamController GenerateBeamPrefabBundleInternal(this Projectile projectile, string defaultSpriteName, tk2dSpriteCollectionData data,
+            tk2dSpriteAnimation animation, string IdleAnimationName, Vector2 colliderDimensions, Vector2 colliderOffsets, string impactVFXAnimationName = null,
+            Vector2? impactVFXColliderDimensions = null, Vector2? impactVFXColliderOffsets = null, string endAnimation = null, Vector2? endColliderDimensions = null,
+            Vector2? endColliderOffsets = null, string muzzleAnimationName = null, Vector2? muzzleColliderDimensions = null, Vector2? muzzleColliderOffsets = null,
+            bool glows = false, bool canTelegraph = false, string beamTelegraphIdleAnimationName = null, string beamStartTelegraphAnimationName = null,
+            string beamEndTelegraphAnimationName = null, float telegraphTime = 1, bool canDissipate = false, string beamDissipateAnimationName = null,
+            string beamStartDissipateAnimationName = null, string beamEndDissipateAnimationName = null, float dissipateTime = 1, bool constructOffsets = true)
+        {
+            try
+            {
+                if (projectile.specRigidbody)
+                    projectile.specRigidbody.CollideWithOthers = false;
+
+                tk2dTiledSprite tiledSprite = projectile.gameObject.GetOrAddComponent<tk2dTiledSprite>();
+
+                tiledSprite.Collection = data;
+                tiledSprite.SetSprite(data, data.GetSpriteIdByName(defaultSpriteName));
+                tk2dSpriteDefinition def = tiledSprite.GetCurrentSpriteDef();
+                def.colliderVertices = new Vector3[]{ 0.0625f * colliderOffsets, 0.0625f * colliderDimensions };
+
+                def.ConstructOffsetsFromAnchor(tk2dBaseSprite.Anchor.MiddleLeft); //NOTE: this seems right, but double check later
+
+                tk2dSpriteAnimator animator = projectile.gameObject.GetOrAddComponent<tk2dSpriteAnimator>();
+                animator._startingSpriteCollection = data;
+                animator.Library = animation;
+                animator.playAutomatically = true;
+                animator.defaultClipId = animation.GetClipIdByName(IdleAnimationName);
+
+                UnityEngine.Object.Destroy(projectile.GetComponentInChildren<tk2dSprite>());
+                projectile.sprite = tiledSprite;
+                projectile.sprite.Collection = data;
+
+                BasicBeamController beamController = projectile.gameObject.GetOrAddComponent<BasicBeamController>();
+                beamController.sprite = tiledSprite;
+                beamController.spriteAnimator = animator;
+                beamController.m_beamSprite = tiledSprite;
+
+                //---------------- Sets up the animation for the main part of the beam
+                beamController.beamAnimation = IdleAnimationName;
+
+                //------------- Sets up the animation for the part of the beam that touches the wall
+
+                if (endAnimation != null && endColliderDimensions != null && endColliderOffsets != null)
+                {
+                    SetupBeamPart(animation, data, endAnimation, (Vector2)endColliderDimensions, (Vector2)endColliderOffsets, constructOffsets: constructOffsets);
+                    beamController.beamEndAnimation = endAnimation;
+                }
+                else
+                {
+                    SetupBeamPart(animation, data, IdleAnimationName, null, null, def.colliderVertices, constructOffsets: constructOffsets);
+                    beamController.beamEndAnimation = IdleAnimationName;
+                }
+
+                //---------------Sets up the animaton for the VFX that plays over top of the end of the beam where it hits stuff
+                if (impactVFXAnimationName != null && impactVFXColliderDimensions != null && impactVFXColliderOffsets != null)
+                {
+                    SetupBeamPart(animation, data, impactVFXAnimationName, (Vector2)impactVFXColliderDimensions, (Vector2)impactVFXColliderOffsets, anchor: tk2dBaseSprite.Anchor.MiddleCenter, constructOffsets: constructOffsets);
+                    beamController.impactAnimation = impactVFXAnimationName;
+                }
+
+                //--------------Sets up the animation for the very start of the beam
+                if (muzzleAnimationName != null && muzzleColliderDimensions != null && muzzleColliderOffsets != null)
+                {
+                    SetupBeamPart(animation, data, muzzleAnimationName, (Vector2)muzzleColliderDimensions, (Vector2)muzzleColliderOffsets, constructOffsets: constructOffsets);
+                    beamController.beamStartAnimation = muzzleAnimationName;
+                }
+                else
+                {
+                    SetupBeamPart(animation, data, IdleAnimationName, null, null, def.colliderVertices, constructOffsets: constructOffsets);
+                    beamController.beamStartAnimation = IdleAnimationName;
+                }
+
+                if (canTelegraph)
+                {
+                    beamController.usesTelegraph = true;
+                    beamController.telegraphAnimations = new BasicBeamController.TelegraphAnims();
+                    if (beamStartTelegraphAnimationName != null)
+                    {
+                        SetupBeamPart(animation, data, beamStartTelegraphAnimationName, Vector2.zero, Vector2.zero, constructOffsets: constructOffsets);
+                        beamController.telegraphAnimations.beamStartAnimation = beamStartTelegraphAnimationName;
+                    }
+                    if (beamTelegraphIdleAnimationName != null)
+                    {
+                        SetupBeamPart(animation, data, beamTelegraphIdleAnimationName, Vector2.zero, Vector2.zero, constructOffsets: constructOffsets);
+                        beamController.telegraphAnimations.beamAnimation = beamTelegraphIdleAnimationName;
+                    }
+                    if (beamEndTelegraphAnimationName != null)
+                    {
+                        SetupBeamPart(animation, data, beamEndTelegraphAnimationName, Vector2.zero, Vector2.zero, constructOffsets: constructOffsets);
+                        beamController.telegraphAnimations.beamEndAnimation = beamEndTelegraphAnimationName;
+                    }
+                    beamController.telegraphTime = telegraphTime;
+                }
+
+                if (canDissipate)
+                {
+                    beamController.endType = BasicBeamController.BeamEndType.Dissipate;
+                    beamController.dissipateAnimations = new BasicBeamController.TelegraphAnims();
+                    if (beamStartDissipateAnimationName != null)
+                    {
+                        SetupBeamPart(animation, data, beamStartDissipateAnimationName, Vector2.zero, Vector2.zero, constructOffsets: constructOffsets);
+                        beamController.dissipateAnimations.beamStartAnimation = beamStartDissipateAnimationName;
+                    }
+                    if (beamDissipateAnimationName != null)
+                    {
+                        SetupBeamPart(animation, data, beamDissipateAnimationName, Vector2.zero, Vector2.zero, constructOffsets: constructOffsets);
+                        beamController.dissipateAnimations.beamAnimation = beamDissipateAnimationName;
+                    }
+                    if (beamEndDissipateAnimationName != null)
+                    {
+                        SetupBeamPart(animation, data, beamEndDissipateAnimationName, Vector2.zero, Vector2.zero, constructOffsets: constructOffsets);
+                        beamController.dissipateAnimations.beamEndAnimation = beamEndDissipateAnimationName;
+                    }
+                    beamController.dissipateTime = dissipateTime;
+                }
+
+                if (glows)
+                    projectile.gameObject.GetOrAddComponent<EmmisiveBeams>();
+                return beamController;
+            }
+            catch (Exception e)
+            {
+                ETGModConsole.Log(e.ToString());
+                return null;
+            }
         }
     }
 }

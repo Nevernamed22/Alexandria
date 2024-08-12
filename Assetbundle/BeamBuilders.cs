98454b9868a66070f49265abb8c313fe27318adc
Patrick Crain <patrickacrain@gmail.com>
2024-08-12 18:17:49 -0400

    Clean up a bunch of beam setup code in BeamBuilders and BeamAPI

--- Assetbundle/BeamBuilders.cs
+++ Assetbundle/BeamBuilders.cs
@@ -4,44 +4,33 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using Alexandria.Misc;
+using Alexandria.ItemAPI;
 
 namespace Alexandria.Assetbundle
 {
     public static class BeamBuilders
     {
         public static BasicBeamController GenerateBeamPrefabBundle(this Projectile projectile, string defaultSpriteName, tk2dSpriteCollectionData data, tk2dSpriteAnimation animation, string IdleAnimationName, Vector2 colliderDimensions, Vector2 colliderOffsets, string impactVFXAnimationName = null, Vector2? impactVFXColliderDimensions = null, Vector2? impactVFXColliderOffsets = null, string endAnimation = null, Vector2? endColliderDimensions = null, Vector2? endColliderOffsets = null, string muzzleAnimationName = null, Vector2? muzzleColliderDimensions = null, Vector2? muzzleColliderOffsets = null, bool glows = false,
-        bool canTelegraph = false, string beamTelegraphIdleAnimationName = null, string beamStartTelegraphAnimationName = null, string beamEndTelegraphAnimationName = null, float telegraphTime = 1,
-        bool canDissipate = false, string beamDissipateAnimationName = null, string beamStartDissipateAnimationName = null, string beamEndDissipateAnimationName = null, float dissipateTime = 1)
-
+            bool canTelegraph = false, string beamTelegraphIdleAnimationName = null, string beamStartTelegraphAnimationName = null, string beamEndTelegraphAnimationName = null, float telegraphTime = 1,
+            bool canDissipate = false, string beamDissipateAnimationName = null, string beamStartDissipateAnimationName = null, string beamEndDissipateAnimationName = null, float dissipateTime = 1)
         {
             try
             {
-                projectile.specRigidbody.CollideWithOthers = false;
-
-
-                float convertedColliderX = colliderDimensions.x / 16f;
-                float convertedColliderY = colliderDimensions.y / 16f;
-                float convertedOffsetX = colliderOffsets.x / 16f;
-                float convertedOffsetY = colliderOffsets.y / 16f;
-
+                if (projectile.specRigidbody)
+                    projectile.specRigidbody.CollideWithOthers = false;
 
                 tk2dTiledSprite tiledSprite = projectile.gameObject.GetOrAddComponent<tk2dTiledSprite>();
 
                 tiledSprite.Collection = data;
                 tiledSprite.SetSprite(data, data.GetSpriteIdByName(defaultSpriteName));
                 tk2dSpriteDefinition def = tiledSprite.GetCurrentSpriteDef();
-                def.colliderVertices = new Vector3[]{
-                    new Vector3(convertedOffsetX, convertedOffsetY, 0f),
-                    new Vector3(convertedColliderX, convertedColliderY, 0f)
-                };
+                def.colliderVertices = new Vector3[]{ 0.0625f * colliderOffsets, 0.0625f * colliderDimensions };
 
-                def.ConstructOffsetsFromAnchor(tk2dBaseSprite.Anchor.MiddleLeft);
+                def.ConstructOffsetsFromAnchor(tk2dBaseSprite.Anchor.MiddleLeft); //NOTE: this seems right, but double check later
 
-                //tiledSprite.anchor = tk2dBaseSprite.Anchor.MiddleCenter;
                 tk2dSpriteAnimator animator = projectile.gameObject.GetOrAddComponent<tk2dSpriteAnimator>();
                 animator._startingSpriteCollection = data;
                 animator.Library = animation;
-                animator.library = animation;
                 animator.playAutomatically = true;
                 animator.defaultClipId = animation.GetClipIdByName(IdleAnimationName);
 
@@ -52,9 +41,7 @@ namespace Alexandria.Assetbundle
                 BasicBeamController beamController = projectile.gameObject.GetOrAddComponent<BasicBeamController>();
                 beamController.sprite = tiledSprite;
                 beamController.spriteAnimator = animator;
-                beamController.spriteAnimator._startingSpriteCollection = data;
                 beamController.m_beamSprite = tiledSprite;
-
 
                 //---------------- Sets up the animation for the main part of the beam
                 beamController.beamAnimation = IdleAnimationName;
@@ -91,60 +78,52 @@ namespace Alexandria.Assetbundle
                     beamController.beamStartAnimation = IdleAnimationName;
                 }
 
-
-
-                if (canTelegraph == true)
+                if (canTelegraph)
                 {
                     beamController.usesTelegraph = true;
                     beamController.telegraphAnimations = new BasicBeamController.TelegraphAnims();
                     if (beamStartTelegraphAnimationName != null)
                     {
-                        SetupBeamPart(animation, data, beamStartTelegraphAnimationName, new Vector2(0, 0), new Vector2(0, 0));
+                        SetupBeamPart(animation, data, beamStartTelegraphAnimationName, Vector2.zero, Vector2.zero);
                         beamController.telegraphAnimations.beamStartAnimation = beamStartTelegraphAnimationName;
                     }
                     if (beamTelegraphIdleAnimationName != null)
                     {
-                        SetupBeamPart(animation, data, beamTelegraphIdleAnimationName, new Vector2(0, 0), new Vector2(0, 0));
+                        SetupBeamPart(animation, data, beamTelegraphIdleAnimationName, Vector2.zero, Vector2.zero);
                         beamController.telegraphAnimations.beamAnimation = beamTelegraphIdleAnimationName;
                     }
                     if (beamEndTelegraphAnimationName != null)
                     {
-                        SetupBeamPart(animation, data, beamEndTelegraphAnimationName, new Vector2(0, 0), new Vector2(0, 0));
+                        SetupBeamPart(animation, data, beamEndTelegraphAnimationName, Vector2.zero, Vector2.zero);
                         beamController.telegraphAnimations.beamEndAnimation = beamEndTelegraphAnimationName;
                     }
                     beamController.telegraphTime = telegraphTime;
                 }
 
-
-                if (canDissipate == true)
+                if (canDissipate)
                 {
                     beamController.endType = BasicBeamController.BeamEndType.Dissipate;
                     beamController.dissipateAnimations = new BasicBeamController.TelegraphAnims();
                     if (beamStartDissipateAnimationName != null)
                     {
-                        SetupBeamPart(animation, data, beamStartDissipateAnimationName, new Vector2(0, 0), new Vector2(0, 0));
+                        SetupBeamPart(animation, data, beamStartDissipateAnimationName, Vector2.zero, Vector2.zero);
                         beamController.dissipateAnimations.beamStartAnimation = beamStartDissipateAnimationName;
                     }
                     if (beamDissipateAnimationName != null)
                     {
-                        SetupBeamPart(animation, data, beamDissipateAnimationName, new Vector2(0, 0), new Vector2(0, 0));
+                        SetupBeamPart(animation, data, beamDissipateAnimationName, Vector2.zero, Vector2.zero);
                         beamController.dissipateAnimations.beamAnimation = beamDissipateAnimationName;
                     }
                     if (beamEndDissipateAnimationName != null)
                     {
-                        SetupBeamPart(animation, data, beamEndDissipateAnimationName, new Vector2(0, 0), new Vector2(0, 0));
+                        SetupBeamPart(animation, data, beamEndDissipateAnimationName, Vector2.zero, Vector2.zero);
                         beamController.dissipateAnimations.beamEndAnimation = beamEndDissipateAnimationName;
                     }
                     beamController.dissipateTime = dissipateTime;
                 }
 
-
                 if (glows)
-                {
-                    EmmisiveBeams emission = projectile.gameObject.GetOrAddComponent<EmmisiveBeams>();
-                    //emission
-
-                }
+                    projectile.gameObject.GetOrAddComponent<EmmisiveBeams>();
                 return beamController;
             }
             catch (Exception e)
@@ -168,76 +147,5 @@ namespace Alexandria.Assetbundle
         {
             Shared.SetupBeamPart(beamAnimation, data, animationName, colliderDimensions, colliderOffsets, overrideVertices, wrapMode, anchor: tk2dBaseSprite.Anchor.MiddleLeft);
         }
-    }
-
-    internal class EmmisiveBeams : MonoBehaviour
-    {
-        public EmmisiveBeams()
-        {
-            this.EmissivePower = 100;
-            this.EmissiveColorPower = 1.55f;
-        }
-        public void Start()
-        {
-            Shader glowshader = ShaderCache.Acquire("Brave/LitTk2dCustomFalloffTiltedCutoutEmissive");
-
-            foreach (Transform transform in base.transform)
-            {
-                if (TransformList.Contains(transform.name))
-                {
-                    tk2dSprite sproot = transform.GetComponent<tk2dSprite>();
-                    if (sproot != null)
-                    {
-                        sproot.usesOverrideMaterial = true;
-                        sproot.renderer.material.shader = glowshader;
-                        sproot.renderer.material.EnableKeyword("BRIGHTNESS_CLAMP_ON");
-                        sproot.renderer.material.SetFloat("_EmissivePower", EmissivePower);
-                        sproot.renderer.material.SetFloat("_EmissiveColorPower", EmissiveColorPower);
-                    }
-                }
-            }
-            this.beamcont = base.GetComponent<BasicBeamController>();
-            BasicBeamController beam = this.beamcont;
-            beam.sprite.usesOverrideMaterial = true;
-            BasicBeamController component = beam.gameObject.GetComponent<BasicBeamController>();
-            bool flag = component != null;
-            bool flag2 = flag;
-            if (flag2)
-            {
-                component.sprite.renderer.material.shader = glowshader;
-                component.sprite.renderer.material.EnableKeyword("BRIGHTNESS_CLAMP_ON");
-                component.sprite.renderer.material.SetFloat("_EmissivePower", EmissivePower);
-                component.sprite.renderer.material.SetFloat("_EmissiveColorPower", EmissiveColorPower);
-            }
-        }
-
-
-        private List<string> TransformList = new List<string>()
-        {
-            "Sprite",
-            "beam impact vfx 2",
-            "beam impact vfx",
-        };
-
-
-        public void Update()
-        {
-            Shader glowshader = ShaderCache.Acquire("Brave/LitTk2dCustomFalloffTiltedCutoutEmissive");
-            Transform trna = base.transform.Find("beam pierce impact vfx");
-            if (trna != null)
-            {
-                tk2dSprite sproot = trna.GetComponent<tk2dSprite>();
-                if (sproot != null)
-                {
-                    sproot.renderer.material.shader = glowshader;
-                    sproot.renderer.material.EnableKeyword("BRIGHTNESS_CLAMP_ON");
-                    sproot.renderer.material.SetFloat("_EmissivePower", EmissivePower);
-                    sproot.renderer.material.SetFloat("_EmissiveColorPower", EmissiveColorPower);
-                }
-            }
-        }
-        private BasicBeamController beamcont;
-        public float EmissivePower;
-        public float EmissiveColorPower;
     }
 }

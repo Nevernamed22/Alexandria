07dce51597f6910a5058a7776e3d2954e8b4f9d7
SpecialAPI <pavlov.andrei.d@gmail.com>
2024-10-27 21:45:50 +0300

    Deprecate TranslationAPI

--- TranslationAPI/TranslationManager.cs
+++ TranslationAPI/TranslationManager.cs
@@ -7,305 +7,140 @@ using MonoMod.RuntimeDetour;
 
 namespace Alexandria.TranslationAPI
 {
-    /// <summary>
-    /// The core class of TranslationAPI that can add new translations.
-    /// </summary>
+    [Obsolete("TranslationAPI is deprecated, use ETGMod.Databases.Strings for translations instead.", true)]
     public static class TranslationManager
     {
-        /// <summary>
-        /// Inits TranslationAPI. You're not required to call this method, it should be called automatically when you try to add a translation.
-        /// </summary>
         public static void Init()
         {
-            if (m_initialized)
-            {
-                return;
-            }
-            translations = new List<Translation>();
-            foreach (StringTableManager.GungeonSupportedLanguages language in Enum.GetValues(typeof(StringTableManager.GungeonSupportedLanguages)))
-            {
-                if (language != StringTableManager.GungeonSupportedLanguages.RUBEL_TEST && language != StringTableManager.GungeonSupportedLanguages.ENGLISH)
-                {
-                    translations.Add(new Translation
-                    {
-                        language = language,
-                        strings = new Dictionary<string, StringTableManager.StringCollection>(),
-                        getTable = () => StringTableManager.CoreTable,
-                        type = StringTableType.Core
-                    });
-                    translations.Add(new Translation
-                    {
-                        language = language,
-                        strings = new Dictionary<string, StringTableManager.StringCollection>(),
-                        getTable = () => StringTableManager.ItemTable,
-                        type = StringTableType.Items
-                    });
-                    translations.Add(new Translation
-                    {
-                        language = language,
-                        strings = new Dictionary<string, StringTableManager.StringCollection>(),
-                        getTable = () => StringTableManager.EnemyTable,
-                        type = StringTableType.Enemies
-                    });
-                    translations.Add(new Translation
-                    {
-                        language = language,
-                        strings = new Dictionary<string, StringTableManager.StringCollection>(),
-                        getTable = () => UITable,
-                        type = StringTableType.UI
-                    });
-                    translations.Add(new Translation
-                    {
-                        language = language,
-                        strings = new Dictionary<string, StringTableManager.StringCollection>(),
-                        getTable = () => StringTableManager.IntroTable,
-                        type = StringTableType.Intro
-                    });
-                    translations.Add(new Translation
-                    {
-                        language = language,
-                        strings = new Dictionary<string, StringTableManager.StringCollection>(),
-                        getTable = () => SynergyTable,
-                        type = StringTableType.Synergy
-                    });
-                }
-            }
-            UIStrings = new CustomStringDBTable(() => UITable);
-            SynergyStrings = new CustomStringDBTable(() => SynergyTable);
-            ETGMod.Databases.Strings.OnLanguageChanged += DoTranslation;
-            SetValueHook = new Hook(typeof(StringDBTable).GetProperty("Item", BindingFlags.Public | BindingFlags.Instance).GetSetMethod(), typeof(TranslationManager).GetMethod("TableSetValueHook", BindingFlags.Static | BindingFlags.NonPublic));
-            m_initialized = true;
         }
 
-        /// <summary>
-        /// Unloads TranslationAPI and removes all added translations.
-        /// </summary>
         public static void Unload()
         {
-            if (!m_initialized)
-            {
-                return;
-            }
-            translations?.Clear();
-            translations = null;
-            UIStrings = null;
-            SynergyStrings = null;
-            ETGMod.Databases.Strings.OnLanguageChanged -= DoTranslation;
-            SetValueHook?.Dispose();
-            m_initialized = false;
-            StringTableManager.SetNewLanguage(StringTableManager.CurrentLanguage, true);
         }
 
-        private static void TableSetValueHook(Action<StringDBTable, string, StringTableManager.StringCollection> orig, StringDBTable self, string key, StringTableManager.StringCollection value)
-        {
-            orig(self, key, value);
-            ForceUpdateTranslation();
-        }
-
-        /// <summary>
-        /// Adds a new translation for <paramref name="item"/>'s name.
-        /// </summary>
-        /// <param name="item">The <see cref="PickupObject"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateItemName(this PickupObject item, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            item.encounterTrackable?.TranslateName(language, translation);
+            item.SetName(language, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="item"/>'s short description.
-        /// </summary>
-        /// <param name="item">The <see cref="PickupObject"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateItemShortDescription(this PickupObject item, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            item.encounterTrackable?.TranslateShortDescription(language, translation);
+            item.SetShortDescription(language, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="item"/>'s long description.
-        /// </summary>
-        /// <param name="item">The <see cref="PickupObject"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateItemLongDescription(this PickupObject item, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            item.encounterTrackable?.TranslateLongDescription(language, translation);
+            item.SetLongDescription(language, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="enemy"/>'s name.
-        /// </summary>
-        /// <param name="enemy">The <see cref="AIActor"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateEnemyName(this AIActor enemy, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            enemy.encounterTrackable?.TranslateName(language, translation);
+            if (enemy == null || enemy.encounterTrackable == null || enemy.encounterTrackable.journalData == null)
+                return;
+
+            ETGMod.Databases.Strings.Enemies.Set(language, enemy.encounterTrackable.journalData.PrimaryDisplayName, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="enemy"/>'s short description.
-        /// </summary>
-        /// <param name="enemy">The <see cref="AIActor"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateEnemyShortDescription(this AIActor enemy, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            enemy.encounterTrackable?.TranslateShortDescription(language, translation);
+            if (enemy == null || enemy.encounterTrackable == null || enemy.encounterTrackable.journalData == null)
+                return;
+
+            ETGMod.Databases.Strings.Enemies.Set(language, enemy.encounterTrackable.journalData.NotificationPanelDescription, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="enemy"/>'s long description.
-        /// </summary>
-        /// <param name="enemy">The <see cref="AIActor"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateEnemyLongDescription(this AIActor enemy, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            enemy.encounterTrackable?.TranslateLongDescription(language, translation);
+            if (enemy == null || enemy.encounterTrackable == null || enemy.encounterTrackable.journalData == null)
+                return;
+
+            ETGMod.Databases.Strings.Enemies.Set(language, enemy.encounterTrackable.journalData.AmmonomiconFullEntry, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="track"/>'s name.
-        /// </summary>
-        /// <param name="track">The <see cref="EncounterTrackable"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateName(this EncounterTrackable track, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            AddTranslation(language, track.journalData.PrimaryDisplayName, translation, StringTableType.Items);
+            if(track == null || track.journalData == null)
+                return;
+
+            var table = track.journalData.IsEnemy ? ETGMod.Databases.Strings.Enemies : ETGMod.Databases.Strings.Items;
+            table.Set(language, track.journalData.PrimaryDisplayName, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="track"/>'s short description.
-        /// </summary>
-        /// <param name="track">The <see cref="EncounterTrackable"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateShortDescription(this EncounterTrackable track, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            AddTranslation(language, track.journalData.NotificationPanelDescription, translation, StringTableType.Items);
+            if (track == null || track.journalData == null)
+                return;
+
+            var table = track.journalData.IsEnemy ? ETGMod.Databases.Strings.Enemies : ETGMod.Databases.Strings.Items;
+            table.Set(language, track.journalData.NotificationPanelDescription, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for <paramref name="track"/>'s long description.
-        /// </summary>
-        /// <param name="track">The <see cref="EncounterTrackable"/> to add the translation to.</param>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="translation">The translated text.</param>
         public static void TranslateLongDescription(this EncounterTrackable track, StringTableManager.GungeonSupportedLanguages language, string translation)
         {
-            AddTranslation(language, track.journalData.AmmonomiconFullEntry, translation, StringTableType.Items);
+            if (track == null || track.journalData == null)
+                return;
+
+            var table = track.journalData.IsEnemy ? ETGMod.Databases.Strings.Enemies : ETGMod.Databases.Strings.Items;
+            table.Set(language, track.journalData.AmmonomiconFullEntry, translation);
         }
 
-        /// <summary>
-        /// Adds a new translation for an individual string.
-        /// </summary>
-        /// <param name="language">The language to which the translation will be applied.</param>
-        /// <param name="key">The string's key</param>
-        /// <param name="value">The string</param>
-        /// <param name="tableType">Type of the table in which <paramref name="value"/> is in.</param>
         public static void AddTranslation(StringTableManager.GungeonSupportedLanguages language, string key, string value, StringTableType tableType)
         {
-            if(translations == null)
+            switch (tableType)
             {
-                Init();
-            }
-            if(language == StringTableManager.GungeonSupportedLanguages.ENGLISH)
-            {
-                switch (tableType)
-                {
-                    case StringTableType.Core:
-                        ETGMod.Databases.Strings.Core.Set(key, value);
-                        break;
-                    case StringTableType.Items:
-                        ETGMod.Databases.Strings.Items.Set(key, value);
-                        break;
-                    case StringTableType.Enemies:
-                        ETGMod.Databases.Strings.Enemies.Set(key, value);
-                        break;
-                    case StringTableType.UI:
-                        UIStrings.Set(key, value);
-                        break;
-                    case StringTableType.Intro:
-                        ETGMod.Databases.Strings.Intro.Set(key, value);
-                        break;
-                    case StringTableType.Synergy:
-                        SynergyStrings.Set(key, value);
-                        break;
-                }
-                return;
-            }
-            foreach(Translation translation in translations)
-            {
-                if(translation.language == language && translation.type == tableType)
-                {
-                    translation.AddStringTranslation(key, value);
-                }
-            }
-        }
-
-        private static void DoTranslation(StringTableManager.GungeonSupportedLanguages lang)
-        {
-            UIStrings.LanguageChanged();
-            SynergyStrings.LanguageChanged();
-            foreach(Translation translation in translations)
-            {
-                translation.UpdateLanguage(lang);
+                case StringTableType.Core:
+                    ETGMod.Databases.Strings.Core.Set(language, key, value);
+                    break;
+                case StringTableType.Items:
+                    ETGMod.Databases.Strings.Items.Set(language, key, value);
+                    break;
+                case StringTableType.Enemies:
+                    ETGMod.Databases.Strings.Enemies.Set(language, key, value);
+                    break;
+                case StringTableType.UI:
+                    ETGMod.Databases.Strings.UI.Set(language switch
+                    {
+                        StringTableManager.GungeonSupportedLanguages.ENGLISH => dfLanguageCode.EN,
+                        StringTableManager.GungeonSupportedLanguages.BRAZILIANPORTUGUESE => dfLanguageCode.PT,
+                        StringTableManager.GungeonSupportedLanguages.FRENCH => dfLanguageCode.FR,
+                        StringTableManager.GungeonSupportedLanguages.GERMAN => dfLanguageCode.DE,
+                        StringTableManager.GungeonSupportedLanguages.ITALIAN => dfLanguageCode.IT,
+                        StringTableManager.GungeonSupportedLanguages.SPANISH => dfLanguageCode.ES,
+                        StringTableManager.GungeonSupportedLanguages.POLISH => dfLanguageCode.PL,
+                        StringTableManager.GungeonSupportedLanguages.RUSSIAN => dfLanguageCode.RU,
+                        StringTableManager.GungeonSupportedLanguages.JAPANESE => dfLanguageCode.JA,
+                        StringTableManager.GungeonSupportedLanguages.KOREAN => dfLanguageCode.KO,
+                        StringTableManager.GungeonSupportedLanguages.RUBEL_TEST => dfLanguageCode.QU,
+                        StringTableManager.GungeonSupportedLanguages.CHINESE => dfLanguageCode.ZH,
+                        _ => dfLanguageCode.EN,
+                    }, key, value);
+                    break;
+                case StringTableType.Intro:
+                    ETGMod.Databases.Strings.Intro.Set(language, key, value);
+                    break;
+                case StringTableType.Synergy:
+                    ETGMod.Databases.Strings.Synergy.Set(language, key, value);
+                    break;
             }
         }
 
-        /// <summary>
-        /// Force updates the translations.
-        /// </summary>
         public static void ForceUpdateTranslation()
         {
-            foreach (Translation translation in translations)
-            {
-                translation.UpdateLanguage();
-            }
         }
 
-        /// <summary>
-        /// The table with all the synergy strings.
-        /// </summary>
-        public static Dictionary<string, StringTableManager.StringCollection> SynergyTable
-        {
-            get
-            {
-                StringTableManager.GetSynergyString("#PRISMATISM_IS_BAD");
-                return typeof(StringTableManager).GetField("m_synergyTable", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null) as Dictionary<string, StringTableManager.StringCollection>;
-            }
-        }
+        public static Dictionary<string, StringTableManager.StringCollection> SynergyTable => StringDB.SynergyTable;
 
-        /// <summary>
-        /// The table with all the UI strings.
-        /// </summary>
         public static Dictionary<string, StringTableManager.StringCollection> UITable
         {
             get
             {
-                StringTableManager.GetSynergyString("#DOWNLOAD_ETG_HARDMODE");
-                return typeof(StringTableManager).GetField("m_uiTable", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null) as Dictionary<string, StringTableManager.StringCollection>;
+                StringTableManager.GetSynergyString("");
+                return StringTableManager.m_uiTable;
             }
         }
 
-        /// <summary>
-        /// The list of all table translations.
-        /// </summary>
-        public static List<Translation> translations;
-        /// <summary>
-        /// The <see cref="CustomStringDBTable"/> for UI strings.
-        /// </summary>
-        public static CustomStringDBTable UIStrings;
-        /// <summary>
-        /// The <see cref="CustomStringDBTable"/> for synergy strings.
-        /// </summary>
-        public static CustomStringDBTable SynergyStrings;
-        private static Hook SetValueHook;
-        private static bool m_initialized;
+        public static List<Translation> translations = new();
+        public static CustomStringDBTable UIStrings = new();
+        public static CustomStringDBTable SynergyStrings = new(ETGMod.Databases.Strings.Synergy);
     }
 }

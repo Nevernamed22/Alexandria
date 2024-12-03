30d1ab2defb6a89a0e2090853f814fd2d923fc47
Patrick Crain <patrickacrain@gmail.com>
2024-12-02 19:16:36 -0500

    Fix hats that vanish on roll not properly sticking to the player when reappearing, and bump version to 0.4.17

--- Module.cs
+++ Module.cs
@@ -28,7 +28,7 @@ namespace Alexandria
     {
         public const string GUID = "alexandria.etgmod.alexandria";
         public const string NAME = "Alexandria";
-        public const string VERSION = "0.4.16";
+        public const string VERSION = "0.4.17";
 
         public void Start()
         {

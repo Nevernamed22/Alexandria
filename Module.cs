aac35d7212752b5f25e77df1e8606584591c3848
Patrick Crain <patrickacrain@gmail.com>
2025-05-01 18:09:37 -0400

    Added nupkg files to .gitignore and bumped version to 0.4.20

--- Module.cs
+++ Module.cs
@@ -28,7 +28,7 @@ namespace Alexandria
     {
         public const string GUID = "alexandria.etgmod.alexandria";
         public const string NAME = "Alexandria";
-        public const string VERSION = "0.4.19";
+        public const string VERSION = "0.4.20";
 
         public void Start()
         {

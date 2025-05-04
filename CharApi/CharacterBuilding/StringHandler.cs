90a10c449698fc5eb277c3049c7ea79e376220cc
Patrick Crain <patrickacrain@gmail.com>
2025-05-04 19:58:55 -0400

    Clean up StringHandler.cs

--- CharApi/CharacterBuilding/StringHandler.cs
+++ CharApi/CharacterBuilding/StringHandler.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Reflection;
 
 using UnityEngine;
 namespace Alexandria.CharacterAPI
@@ -11,33 +9,14 @@ namespace Alexandria.CharacterAPI
 
     public static class StringHandler
     {
-        private static FieldInfo strings = typeof(StringTableManager.ComplexStringCollection).GetField("strings", BindingFlags.NonPublic | BindingFlags.Instance);
-        private static FieldInfo singleString = typeof(StringTableManager.SimpleStringCollection).GetField("singleString", BindingFlags.NonPublic | BindingFlags.Instance);
-        private static FieldInfo dfStrings = typeof(dfLanguageManager).GetField("strings", BindingFlags.NonPublic | BindingFlags.Instance);
         public static Dictionary<string, string> customStringDictionary = new Dictionary<string, string>();
-
-        private static string[] fields =
-        {
-            "m_coreTable", //good 
-            "m_backupCoreTable", //good
-        };
-
 
         public static void AddStringDefinition(string key, string value)
         {
-            foreach (string field in fields)
-            {
-                FieldInfo fi = typeof(StringTableManager).GetField(field, BindingFlags.NonPublic | BindingFlags.Static);
-                var dictionary = (Dictionary<string, StringTableManager.StringCollection>)fi.GetValue(null);
-
-                if (dictionary == null) continue;
-
-                var collection = new StringTableManager.SimpleStringCollection();
-                collection.AddString(value, 1);
-
-                if (!dictionary.ContainsKey(key))
-                    dictionary.Add(key, collection);
-            }
+            if (StringTableManager.m_coreTable is var coretable && !coretable.ContainsKey(key))
+                coretable.Add(key, new StringTableManager.SimpleStringCollection(){singleString = value});
+            if (StringTableManager.m_backupCoreTable is var backupCoretable && !backupCoretable.ContainsKey(key))
+                backupCoretable.Add(key, new StringTableManager.SimpleStringCollection(){singleString = value});
         }
 
         public static void AddDFStringDefinition(string key, string value)

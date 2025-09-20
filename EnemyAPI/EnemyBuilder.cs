6496a38d0936afce75380d49426faec5e7aff25a
Patrick Crain <patrickacrain@gmail.com>
2025-09-20 12:57:55 -0400

    Clean up EnemyBuilder.cs and replace hook with equivalent Harmony patch

--- EnemyAPI/EnemyBuilder.cs
+++ EnemyAPI/EnemyBuilder.cs
@@ -10,9 +10,11 @@ using MonoMod.RuntimeDetour;
 using Brave.BulletScript;
 using DirectionType = DirectionalAnimation.DirectionType;
 using FlipType = DirectionalAnimation.FlipType;
+using HarmonyLib;
 
 namespace Alexandria.EnemyAPI
 {
+    [HarmonyPatch]
     public static class EnemyBuilder
     {
         private static GameObject behaviorSpeculatorPrefab;
@@ -40,18 +42,14 @@ namespace Alexandria.EnemyAPI
             GameObject.DontDestroyOnLoad(behaviorSpeculatorPrefab);
             FakePrefab.MarkAsFakePrefab(behaviorSpeculatorPrefab);
             behaviorSpeculatorPrefab.SetActive(false);
-
-            Hook enemyHook = new Hook(
-                typeof(EnemyDatabase).GetMethod("GetOrLoadByGuid", BindingFlags.Public | BindingFlags.Static),
-                typeof(EnemyBuilder).GetMethod("GetOrLoadByGuid")
-            );
         }
 
-        public static AIActor GetOrLoadByGuid(Func<string, AIActor> orig, string guid)
+        [HarmonyPatch(typeof(EnemyDatabase), nameof(EnemyDatabase.GetOrLoadByGuid))]
+        [HarmonyPostfix]
+        private static void EnemyDatabaseGetOrLoadByGuidPatch(EnemyDatabase __instance, string guid, ref AIActor __result)
         {
             if (Dictionary.TryGetValue(guid, out GameObject companion))
-                return companion.GetComponent<AIActor>();
-            return orig(guid);
+                __result = companion.GetComponent<AIActor>();
         }
 
         public static GameObject BuildPrefab(string name, string guid, string defaultSpritePath, IntVector2 hitboxOffset, IntVector2 hitBoxSize, bool HasAiShooter)

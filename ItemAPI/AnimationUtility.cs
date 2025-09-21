c08204adc4c23627af221d0f1426280391589d27
Patrick Crain <patrickacrain@gmail.com>
2025-09-21 10:03:56 -0400

    Clean up AddAnimationToObject()

--- ItemAPI/AnimationUtility.cs
+++ ItemAPI/AnimationUtility.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using UnityEngine;
+using Alexandria.Misc;
 
 namespace Alexandria.ItemAPI
 {
@@ -11,31 +12,25 @@ namespace Alexandria.ItemAPI
     {
         public static void AddAnimationToObject(this GameObject target, tk2dSpriteCollectionData spriteCollection, string animationName, List<string> spritePaths, int fps, Vector2 colliderDimensions, Vector2 colliderOffsets, tk2dBaseSprite.Anchor anchor, tk2dSpriteAnimationClip.WrapMode wrapMode, bool isDefaultAnimation = false, Assembly assembly = null)
         {
-            tk2dSpriteAnimator animator = target.GetOrAddComponent<tk2dSpriteAnimator>();
-            tk2dSpriteAnimation animation = target.GetOrAddComponent<tk2dSpriteAnimation>();
-            animation.clips = new tk2dSpriteAnimationClip[0];
-            animator.Library = animation;
-
-            tk2dSpriteAnimationClip clip = new tk2dSpriteAnimationClip() { name = animationName, frames = new tk2dSpriteAnimationFrame[0], fps = fps };
-
-            List<tk2dSpriteAnimationFrame> frames = new List<tk2dSpriteAnimationFrame>();
-            foreach (string path in spritePaths)
+            tk2dSpriteAnimationFrame[] frames = new tk2dSpriteAnimationFrame[spritePaths.Count];
+            for (int i = 0; i < spritePaths.Count; ++i)
             {
-                tk2dSpriteCollectionData collection = spriteCollection;
-                int frameSpriteId = SpriteBuilder.AddSpriteToCollection(path, collection, assembly);
-                tk2dSpriteDefinition frameDef = collection.spriteDefinitions[frameSpriteId];
+                int frameSpriteId = SpriteBuilder.AddSpriteToCollection(spritePaths[i], spriteCollection, assembly);
+                tk2dSpriteDefinition frameDef = spriteCollection.spriteDefinitions[frameSpriteId];
 
                 frameDef.colliderVertices = new Vector3[]{
-                    new Vector3(colliderOffsets.x/16, colliderOffsets.y/16, 0f),
+                    new Vector3(colliderOffsets.x / 16, colliderOffsets.y / 16, 0f),
                     new Vector3((colliderDimensions.x / 16), (colliderDimensions.y / 16), 0f)
                 };
 
                 frameDef.ConstructOffsetsFromAnchor(anchor);
-                frames.Add(new tk2dSpriteAnimationFrame { spriteId = frameSpriteId, spriteCollection = collection });
+                frames[i] = new tk2dSpriteAnimationFrame { spriteId = frameSpriteId, spriteCollection = spriteCollection };
             }
-            clip.frames = frames.ToArray();
-            clip.wrapMode = wrapMode;
-            animation.clips = animation.clips.Concat(new tk2dSpriteAnimationClip[] { clip }).ToArray();
+
+            tk2dSpriteAnimator animator = target.GetOrAddComponent<tk2dSpriteAnimator>();
+            tk2dSpriteAnimation animation = target.GetOrAddComponent<tk2dSpriteAnimation>();
+            animator.Library = animation;
+            animation.clips = new tk2dSpriteAnimationClip[1] { new tk2dSpriteAnimationClip() { name = animationName, fps = fps, frames = frames, wrapMode = wrapMode } };
 
             if (isDefaultAnimation)
             {

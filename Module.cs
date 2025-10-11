07bdae1ca1a16d88398b60ae3b7d87665bfd24e4
Patrick Crain <patrickacrain@gmail.com>
2025-10-11 11:36:50 -0400

    Bump version to 0.5.0

--- Module.cs
+++ Module.cs
@@ -27,7 +27,7 @@ namespace Alexandria
     {
         public const string GUID = "alexandria.etgmod.alexandria";
         public const string NAME = "Alexandria";
-        public const string VERSION = "0.4.25";
+        public const string VERSION = "0.5.0";
 
         public void Start()
         {
